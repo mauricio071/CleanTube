85f5607d0eba1bbb3081ce38ff9d0004cd862b2b
Maurício Naoki <naokimau@gmail.com>
2024-09-23 16:44:11 -0300

    fix

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,3 +1,4 @@
 export default defineEventHandler((event) => {
     console.log("Acessando: " + getRequestURL(event));
+    console.log(process.env.NODE_ENV);
 })
--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,27 +1,23 @@
 import pg from "pg"
 
-//Para local
 export const postgresClient = () => {
-    const client = new pg.Pool({
-        host: process.env.DB_HOST,
-        port: 5432,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
-    });
+    const client = new pg.Pool(
+        process.env.NODE_ENV === 'production'
+            ? {
+                // Para deploy
+                connectionString: process.env.POSTGRES_URL
+            }
+            : {
+                // Para local
+                host: process.env.DB_HOST,
+                port: 5432,
+                database: process.env.DB_DATABASE,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD
+            }
+    );
 
     return {
         client,
     };
-}
-
-//Para deploy
-// export const postgresClient = () => {
-//     const client = new pg.Pool({
-//         connectionString: process.env.POSTGRES_URL
-//     });
-
-//     return {
-//         client,
-//     };
-// }
+};
